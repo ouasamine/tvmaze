8311d921ecdc27871fff69fe3953ac3662582017
Amine <ouas.amine@outlook.com>
2022-10-19 00:42:09 +0100

    Add more details to popup windows

--- modules/shows-fetch.js
+++ modules/shows-fetch.js
@@ -1,10 +1,6 @@
-export default async function fetchShow(endpoint, id, embed) {
-  if (embed) {
-    embed = `?embed=${embed}`;
-  } else {
-    embed = '';
-  }
-  const fetching = await fetch(`https://api.tvmaze.com/${endpoint}/${id}${embed}`)
+export default function fetchShow(endpoint, id, embed = '') {
+  
+  const fetching = fetch(`https://api.tvmaze.com/${endpoint}/${id}${embed}`)
     .then((response) => response.json());
   return fetching;
 }
