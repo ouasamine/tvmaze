2f3e1ebcdfda60f5e671939502f6936dda1e7f8f
Amine <ouas.amine@outlook.com>
2022-10-19 00:55:06 +0100

    Fix linters

--- modules/shows-fetch.js
+++ modules/shows-fetch.js
@@ -1,5 +1,4 @@
 export default function fetchShow(endpoint, id, embed = '') {
-  
   const fetching = fetch(`https://api.tvmaze.com/${endpoint}/${id}${embed}`)
     .then((response) => response.json());
   return fetching;
