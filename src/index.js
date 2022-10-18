91cca50a48034f58bbef381c79292aa6b185cb91
Amine <ouas.amine@outlook.com>
2022-10-18 09:08:38 +0100

    Fix cards width

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import fetchShow from '../modules/shows-fetch.js';
 import './style.css';
 
 const container = document.querySelector('#shows-preview');
-const showsIds = [153021, 367506, 266189, 328634, 72108];
+const showsIds = [153021, 367506, 266189, 328634, 72108, 164981];
 showsIds.forEach((show) => {
   fetchShow(show).then((res) => {
     container.innerHTML += `<div class="movie-holder">
     <img src="${res.image.medium}">
     <section class='section-underImage'>
-      <p>${res.name}</p>
+      <p class="show-title">${res.name}</p>
       <div class="like-button">
         <button class="like-button"><i class="fa-regular fa-heart"></i></button>
         <p>2 likes</p>
