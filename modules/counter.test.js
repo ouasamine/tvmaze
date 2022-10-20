2a60f524888d9ed25b03e81b8c2f5c4e2487315f
Ahmed Nazir Musah <ahmed999nazir@gmail.com>
2022-10-20 10:17:49 +0000

    added test for the counter function

--- modules/counter.test.js
+++ modules/counter.test.js
@@ -1,7 +1,23 @@
-const counter = require('./counter.js');
+/**
+ * @jest-environment jsdom
+ */
 
-const showsIds = [73, 33352, 69, 21845, 60, 100];
+import counter from './counter.js';
 
-test('total number of muvies', () => {
-  expect(counter(showsIds)).toBe(showsIds.length);
+const container1 = document.createElement('div');
+for (let i = 0; i < 5; i += 1) {
+  const container = document.createElement('div');
+  container1.append(container);
+}
+const container2 = document.createElement('div');
+for (let i = 0; i < 13; i += 1) {
+    const container = document.createElement('div');
+    container2.append(container);
+}
+const container3 = document.createElement('div');
+
+test('total number of movies', () => {
+  expect(counter(container1)).toBe(5);
+  expect(counter(container2)).toBe(13);
+  expect(counter(container3)).toBe(0);
 });
